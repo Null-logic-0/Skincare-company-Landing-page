69d92c5b68a6d2344eee5801032fc8744d6282b8
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-05-06 21:57:03 +0400

    feat: added new sections,added burger menu ,and feedback section

---
+++ scripts/burger-menu.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("burgerMenu").style.width = "50%";
+  }
+  
+  function closeNav() {
+    document.getElementById("burgerMenu").style.width = "0%";
+  }
